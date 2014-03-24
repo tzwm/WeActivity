4e8b66ad8c8de822cb1080f22b1d6f057f40a571
tzwm <wangjunzhe4444@gmail.com>
2014-03-25 04:03:06 +0800

    can generate controller and client html from ppt uploaded, add socket.io to sync and have terrible code which need refactoring.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * Module dependencies.
  */
 
+var fs = require('fs');
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -10,6 +11,8 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -35,7 +38,33 @@ app.get('/', routes.index);
 app.get('/dashboard', routes.dashboard);
 app.get('/upload', routes.upload);
 app.post('/upload', routes.doUpload);
+app.get('/client', routes.client);
+app.get('/controller', routes.controller);
 
-http.createServer(app).listen(app.get('port'), function(){
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+io.sockets.on('connection', function(socket) {
+  socket.on('change', function(data) {
+    console.log('haha');
+    console.log(data.indexh);
+    io.sockets.emit('goto', 
+      {indexh: data.indexh,
+       indexv: data.indexv});
+  });
+});
+
+app.locals.addSections = function() {
+  console.log('hello');
+  var r = "";
+  fs.readdir('./public/images/tmp/', function(err, files){
+    files.forEach(function(file) {
+      r = r + "<section data-background=\"images/tmp/"+file+"\"></section>"; 
+    });
+  });
+
+  return r;
+};
+
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@
 
 var util = require('util');
 var formidable = require('formidable');
+var ppt2png = require('ppt2png');
+var fs = require('fs');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
@@ -28,5 +30,50 @@ exports.doUpload = function(req, res){
     res.writeHead(200, {'content-type': 'text/plain'});
     res.write('received upload:\n\n');
     res.end(util.inspect({fields: fields, files: files}));
+
+    ppt2png(files.test.path, './public/images/tmp/img', function(err){
+      if(err != null){
+        console.log(err);
+      } else {
+        fs.unlink('./public/images/tmp/img.pdf', function(err){
+          if(err != null){
+            console.log(err);
+          } else {
+            console.log('success');
+          }
+        });
+      }
+    });
   });
 };
+
+exports.client = function(req, res){
+  var r = "";
+  
+  fs.readdir('./public/images/tmp/', function(err, files){
+    files.forEach(function(file) {
+      console.log(file);
+      r = r + "<section data-background=\"images/tmp/"+file+"\"></section>"; 
+    });
+
+    console.log(r);
+    res.render('slide_layouts/reveal_client', { title: 'client', data: r });
+  });
+
+};
+
+exports.controller = function(req, res){
+  var r = "";
+  
+  fs.readdir('./public/images/tmp/', function(err, files){
+    files.forEach(function(file) {
+      console.log(file);
+      r = r + "<section data-background=\"images/tmp/"+file+"\"></section>"; 
+    });
+
+    console.log(r);
+    res.render('slide_layouts/reveal_controller', { title: 'controller', data: r });
+  });
+
+};
+
